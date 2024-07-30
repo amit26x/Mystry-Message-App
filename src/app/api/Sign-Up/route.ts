3adf9ef7857c5e7995dc2e47f516b84fb9a23326
Amit Gupta <amit2606.gupta@gmail.com>
2024-07-30 17:31:54 +0530

    Implement src layout

---
+++ src/app/api/Sign-Up/route.ts
@@ -0,0 +1,9 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail.";
+
+export async function POST(request: Request){
+    
+}
