48ede83b007b42471880f11f3bd1501856af67ff
Amit Gupta <amit2606.gupta@gmail.com>
2024-07-31 22:50:44 +0530

    implement token

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -50,12 +50,21 @@ export const authOptions: NextAuthOptions = {
     ],
     callbacks: {
         async session({ session, token }) {
+            if(token){
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+
+            }
             return session
         },
         async jwt({ token, user }) {
             if(user){
                 token._id = user._id?.toString()
-                token.isVerified = user.isverified
+                token.isVerified = user.isVerified;
+                token.isAcceptinMessages = user.isAcceptingMessages; 
+                token.username = user.username;
             }
             return token
         },
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import 'next-auth'
+import { DefaultSession } from 'next-auth'
 
 
 declare module 'next-auth' {
@@ -6,5 +7,24 @@ declare module 'next-auth' {
         _id?: string
         isVerified?: boolean
         isAcceptingMessages?: boolean
+        username?: string
+
+    }
+    interface Session{
+        user: {
+            _id?: string
+            isVerified?: boolean
+            isAcceptingMessages?: boolean
+            username?: string
+        } & DefaultSession['user']
+    }
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        _id?: string
+        isVerified?: boolean
+        isAcceptingMessages?: boolean
+        username?: string
     }
 }
